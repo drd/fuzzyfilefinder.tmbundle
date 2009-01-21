5709341fc3b61e6e54083ee406d5bf8023c0b1ac
Eric O'Connell <eric@roundpegdesigns.com>
2009-01-21 15:36:55 -0800

    Larger ceiling + TextMate::UI.request_item
    [add] cieling of 100_000
    [remove] HTML output
    [add] call to TextMate::UI.request_item

--- Support/lib/file_finder.rb
+++ Support/lib/file_finder.rb
@@ -2,6 +2,8 @@
 # Amiel Martin
 # 2009-01-21
 #
+# modified by Eric O'Connell to use TextMate::UI.request_item
+#
 # a proof-of-concept to bring FuzzyFileFinder (http://github.com/jamis/fuzzy_file_finder/tree/master)
 # back to textmate. FuzzyFileFinder is a (somewhat improved) implementation of TextMate's "cmd-T"
 # functionality written by Jamis Buck. This bundle however isn't quite as useful as TextMate's "cmd-T"
@@ -33,55 +35,12 @@ module FileFinder
         return nil
       end
       
-      fff = FuzzyFileFinder.new(project_path)
+      fff = FuzzyFileFinder.new(project_path, 100_000)
       matches = fff.find(search_string)
-        
-      TextMate::HTMLOutput.show(:title => 'Files', :sub_title => "Files matching \"#{search_string}\"", :html_head => head_stuff) do |io|
-        if matches.empty?
-          io << "<p>No matches found for \"#{search_string}\"."
-        else
-          io << '<div id="wrapper">'
-          matches.sort{|b,a| a[:score] <=> b[:score] }.each do |match|
-            next if TextMate::ProjectFileFilter.new.binary?(match[:path])
-            io << output_for_match(match)
-          end
-          io << '</div>'
-        end
-      end
+
+      selected = TextMate::UI.request_item(:items => matches.sort{|b,a| a[:score] <=> b[:score] }.map{|m| m[:path].gsub(ENV['TM_PROJECT_DIRECTORY'], '') })
+      TextMate.go_to(:file => "#{ENV['TM_PROJECT_DIRECTORY']}#{selected}")
     end
-    
-    private
-    def highlight(str)
-      str.gsub(/\(([^(]+)\)/, '<span class="highlight">\1</span>')
-    end
-    
-    def url_for(path)
-      "txmt://open/?url=file://#{path}" # "&line=#{line}"
-    end
-    
-    def output_for_match(match)
-      path = match[:path]
-      highlighted_path = highlight(match[:highlighted_path])
-      score_width = (match[:score].to_f * 100).to_i
-      html = <<-HTML
-        <div class="file">
-          <div class="score_wrapper"><div class="score" style="width: #{score_width}%;"></div></div>
-          <a href="#{url_for(path)}">#{highlighted_path}</a>
-        </div>
-      HTML
-      return html
-    end
-    
-    def head_stuff
-      return <<-HTML
-        <style type="text/css">
-          .file{ margin: 0.25em 0; }
-          .file a{ padding-left: 1.25em; display: block; width: 85%; overflow: hidden; }
-          .file .score_wrapper{ float:left; width: 10%; height: 1em; border:1px solid #ccc; }
-          .file .score{ background-color: #494949; width: 1%; height: 100%; }
-          .highlight{ color: #fff; background-color: #494949; }
-        </style>
-      HTML
-    end
+
   end
 end
